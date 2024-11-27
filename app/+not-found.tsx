a1f0a5ed3267b489108afd9ff7b2933bbe81c6e4
Jake.Whittaker <jaffajake25@gmail.com>
2024-11-27 13:41:30 +0000

    Layout base

---
+++ app/+not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const errorPage = () => {
+  return (
+    <View>
+      <Text>Error page not found</Text>
+    </View>
+  );
+};
+
+export default errorPage;
---
+++ components/EventsList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const EventsList = () => {
+  return <View></View>;
+};
+
+export default EventsList;
