95aee753c5b976b7df04bfd263c0e56caec2a653
Jake.Whittaker <jaffajake25@gmail.com>
2024-11-28 15:22:21 +0000

    Adds times to event card and change card layout

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,15 +1,23 @@
 import { Card, Title } from "react-native-paper";
 
 import type { Event } from "./EventsList";
+import { Text } from "react-native";
 
-const EventCard = ({ event }: { event: Event }) => (
-  <Card>
-    <Card.Cover source={{ uri: event.xlargeimageurl }} />
-    <Card.Title title={event.eventname} subtitle={event.date} />
-    <Card.Content>
-      <Title>{event.venue.name}</Title>
-    </Card.Content>
-  </Card>
-);
+const EventCard = ({ event }: { event: Event }) => {
+  console.log(event);
+
+  return (
+    <Card>
+      <Card.Cover source={{ uri: event.xlargeimageurl }} />
+      <Card.Content>
+        <Title>{event.eventname}</Title>
+        <Text>{event.date}</Text>
+        <Text>
+          {event.openingtimes.doorsopen} - {event.openingtimes.doorsclose}
+        </Text>
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default EventCard;
