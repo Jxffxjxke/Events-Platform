ffea79072c5ab5c8bdf5740b17c481d960f7761b
Jake.Whittaker <jaffajake25@gmail.com>
2024-12-18 10:36:33 +0000

    description modal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,43 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
-import {useSession } from "../../context/SessionContext";
+import { useSession } from "../../context/SessionContext";
 import { useLayoutEffect } from "react";
 
 export default function App() {
   const session = useSession();
-  
 
-  useLayoutEffect(() => {
-  }, [session]);
+  useLayoutEffect(() => {}, [session]);
 
   return (
-      <Tabs>
-        <Tabs.Screen
-          name="Home"
-          options={{
-            title: "Home",
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <FontAwesome size={28} name="home" color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="MyEvents"
-          options={{
-            title: "MyEvents",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome size={28} name="list" color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="Auth"
-          options={{
-            title: session ? "Account" : "Log In",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome size={28} name="user" color={color} />
-            ),
-          }}
-        />
-      </Tabs>
+    <Tabs>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="MyEvents"
+        options={{
+          title: "MyEvents",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="list" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Auth"
+        options={{
+          title: session ? "Account" : "Log In",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
