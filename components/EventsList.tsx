4247496a84b5f6e6daaacb5c8c2a03a0538134ae
Jake.Whittaker <jaffajake25@gmail.com>
2024-12-21 13:27:59 +0000

    adds margin to events list

--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -1,26 +1,30 @@
-import { FlatList } from "react-native";
+import { FlatList, View, ViewStyle, StyleProp } from "react-native";
 import EventCard from "./EventCard";
 import { useEffect, useState } from "react";
 import { getEventsList } from "@/utils/getEventsList";
 import { Event } from "@/types/Event";
 
-const EventsList = () => {
+interface EventsListProps {
+  style?: StyleProp<ViewStyle>; 
+}
+
+const EventsList: React.FC<EventsListProps> = ({ style }) => {
   const [eventsArray, setEventsArray] = useState<Event[]>([]);
 
   useEffect(() => {
-    getEventsList().then(( results ) => {
+    getEventsList().then((results) => {
       setEventsArray(results);
     });
   }, []);
 
   return (
-    <>
+    <View style={style}>
       <FlatList
         data={eventsArray}
         renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={(item, index) => `${item.title}-${index}`}
       />
-    </>
+    </View>
   );
 };
 
